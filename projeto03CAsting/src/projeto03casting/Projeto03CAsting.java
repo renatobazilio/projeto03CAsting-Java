2925bb46aed9ba1cec27bb9ddb4c760b0c2980f9
renatobazilio <54293175+renatobazilio@users.noreply.github.com>
2019-08-19 19:48:22 -0300

    Inicialização do projeto03CAsting

---
+++ projeto03CAsting/src/projeto03casting/Projeto03CAsting.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto03casting;
+
+/**
+ *
+ * @author aluno
+ */
+public class Projeto03CAsting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        double pi = 3.14;
+        int i = (int) pi; // Casting = SE MOLDE A....
+        System.out.println("Valor de i: " + i);
+        System.out.println("Valor de pi " + pi);
+        
+        float j = 3.14f;
+        float k = 3;
+        float m = (float) pi; // Casting
+        
+        
+        
+    }
+    
+}
